65dc7e23cff213845db269ab59b25c2e6d006d77
awalias <antwilson@hotmail.co.uk>
2021-08-18 16:16:56 +0800

    chore: adds default x-version-info header

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,7 @@
+const { version } = require('./package.json');
 export const GOTRUE_URL = 'http://localhost:9999'
 export const AUDIENCE = ''
-export const DEFAULT_HEADERS = {}
+export const DEFAULT_HEADERS = { 'x-client-info' : `gotrue-js/${version}` }
 export const EXPIRY_MARGIN = 60 * 1000
 export const STORAGE_KEY = 'supabase.auth.token'
 export const COOKIE_OPTIONS = {
