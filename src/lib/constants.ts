5a512c0590ba5bcd23cbc69de74a4f1480dcd33f
awalias <antwilson@hotmail.co.uk>
2021-08-18 16:59:01 +0800

    fix: test with genversion

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,8 @@
-const { version } = require('./package.json');
+const { version } = require('./version')
+
 export const GOTRUE_URL = 'http://localhost:9999'
 export const AUDIENCE = ''
-export const DEFAULT_HEADERS = { 'x-client-info' : `gotrue-js/${version}` }
+export const DEFAULT_HEADERS = { 'x-client-info': `gotrue-js/${version}` }
 export const EXPIRY_MARGIN = 60 * 1000
 export const STORAGE_KEY = 'supabase.auth.token'
 export const COOKIE_OPTIONS = {
